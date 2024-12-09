40412573a1b5863ff5aa95c9c90c261f83ea3220
MartinezHelenYuE <MartinezHelenYuE@gmail.com>
2024-12-09 19:39:11 +0500

    321

--- script.js
+++ script.js
@@ -1,40 +1,27 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const mainMenu = document.getElementById('mainMenu');
     const game = document.getElementById('game');
-    const startEasyButton = document.getElementById('startEasyButton');
-    const startMediumButton = document.getElementById('startMediumButton');
-    const startHardButton = document.getElementById('startHardButton');
+    const startGameButton = document.getElementById('startGameButton');
     const backToMenuButton = document.getElementById('backToMenuButton');
     const gameArea = document.getElementById('gameArea');
     const scoreDisplay = document.getElementById('score');
     let score = 0;
-    let difficulty = 'easy';
-    const records = {
-        easy: localStorage.getItem('easyRecord') || 0,
-        medium: localStorage.getItem('mediumRecord') || 0,
-        hard: localStorage.getItem('hardRecord') || 0
-    };
 
-    startEasyButton.addEventListener('click', () => startGame('easy'));
-    startMediumButton.addEventListener('click', () => startGame('medium'));
-    startHardButton.addEventListener('click', () => startGame('hard'));
+    startGameButton.addEventListener('click', () => {
+        mainMenu.style.display = 'none';
+        game.style.display = 'block';
+        startGame();
+    });
 
     backToMenuButton.addEventListener('click', () => {
         game.style.display = 'none';
         mainMenu.style.display = 'block';
-        if (score > records[difficulty]) {
-            records[difficulty] = score;
-            localStorage.setItem(`${difficulty}Record`, score);
-        }
         score = 0;
         scoreDisplay.textContent = score;
         gameArea.innerHTML = '';
     });
 
-    function startGame(selectedDifficulty) {
-        difficulty = selectedDifficulty;
-        mainMenu.style.display = 'none';
-        game.style.display = 'block';
+    function startGame() {
         createTarget();
     }
 
@@ -49,23 +36,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
             score++;
             scoreDisplay.textContent = score;
             gameArea.removeChild(target);
-            setTimeout(createTarget, 500); // Добавляем задержку перед созданием нового круга
+            createTarget();
         });
 
         setTimeout(() => {
             if (gameArea.contains(target)) {
                 gameArea.removeChild(target);
-                setTimeout(createTarget, 500); // Добавляем задержку перед созданием нового круга
+                createTarget();
             }
-        }, getTimeout());
-    }
-
-    function getTimeout() {
-        switch (difficulty) {
-            case 'easy': return 1500;
-            case 'medium': return 1000;
-            case 'hard': return 500;
-            default: return 1000;
-        }
+        }, 1000);
     }
 });
